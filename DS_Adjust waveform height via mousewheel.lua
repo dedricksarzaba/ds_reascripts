4257b129442092ad5e4d0464ffc116c31950a5b1
dedricksarzaba <dedrick.sarzaba@gmail.com>
2021-02-16 23:00:42 -0800

    Create DS_Adjust waveform height via mousewheel.lua

---
+++ DS_Adjust waveform height via mousewheel.lua
@@ -0,0 +1,29 @@
+--[[
+ * ReaScript Name: Adjust waveform height via mousewheel
+ * Author: Dedrick Sarzaba
+ * REAPER: 6.23
+ * About: Changes waveform (peaks) view height via mousewheel.
+--]]
+
+
+-- MAIN --
+
+function main()
+
+  is_new,name,sec,cmd,rel,res,val = reaper.get_action_context()
+  
+  if val > 0 then
+    reaper.Main_OnCommand(40155,0)
+  else
+    reaper.Main_OnCommand(40156,0)
+  end
+  
+end
+
+-- INIT --
+reaper.Undo_BeginBlock()
+
+main()
+
+reaper.Undo_EndBlock("Adjust waveform height via mousewheel", -1)
+
